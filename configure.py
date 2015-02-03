bd2e0cd56c6df4d6769eeac79e5948eb6dac9979
pkhopper <letian.llt@live.com>
2015-02-03 20:21:40 +0800

    new

--- configure.py
+++ configure.py
@@ -13,8 +13,10 @@ _src_path = pabspath("src")
 _bdbbox_path = pabspath("src/bdbbox")
 _pbbox_path = pabspath("src/pbbox")
 _protoc_bin = pabspath("vsprojects/pbbox/protoc.exe")
-_protoc_cpp_out = "--cpp_out=%s" % (pabspath("./"))
-_protoc_proto_path = pabspath(pjoin(_pbbox_path, "proto"))
+_proto_path = pabspath(pjoin(_pbbox_path, "proto"))
+_messages_path = pabspath(pjoin(_pbbox_path, "messages"))
+_protoc_cpp_out = "--cpp_out=%s" % (_messages_path)
+_protoc_proto_path = "--proto_path=%s" % (_proto_path)
 
 def _exec_bin(*cmd_args):
     print cmd_args
@@ -23,8 +25,8 @@ def _exec_bin(*cmd_args):
 
 def compile_proto_files():
     print("compile_proto_files:")
-    proto_files = [pjoin(_protoc_proto_path, f) for f in os.listdir(_protoc_proto_path) if f.endswith(".proto")]
-    _exec_bin(_protoc_bin, _protoc_cpp_out, proto_files)
+    proto_files = [pjoin(_proto_path, f) for f in os.listdir(_proto_path) if f.endswith(".proto")]
+    _exec_bin(_protoc_bin, _protoc_cpp_out, proto_files, _protoc_proto_path)
     print("compile_proto_files finish: %d files compiled." % (len(proto_files)))
 
 def main():
--- src/pbbox/main.cpp
+++ src/pbbox/main.cpp
@@ -1,8 +1,65 @@
-#include "proto/test.pb.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "messages/mytype.pb.h"
+#include <google/protobuf/message.h>
+
+using namespace google;
+using namespace std;
+
+
+int serialize(const google::protobuf::Message& msg, void* pdata, unsigned int nsize)
+{
+	if (msg.SerializeToArray(pdata, nsize)) {
+		return msg.ByteSize();
+	}
+	return -1;
+}
+
+int unserialize(google::protobuf::Message& msg, void* pdata, unsigned int nsize)
+{
+	if (msg.ParseFromArray(pdata, nsize)) {
+		return msg.ByteSize();
+	}
+	return -1;
+}
 
 int main(int argc, char* argv[])
 {
+	int nsize = 1024*8;
+	char buffer[1024*8] = {0};
+	string name = "binfile";
+	fstream bf(name, ios::in|ios::binary);
+	MyType myt;
+	int ret = -1;
 
+	if (bf.is_open()) {
+		cout<<"read from:"<<name<<endl;
+		do 
+		{
+			bf.read(buffer, nsize);
+		} while (!bf.eof());
+		if (myt.ParseFromArray(buffer, bf.gcount())) {
+			;	
+		}
+		myt.PrintDebugString();
+		string strmsg = myt.strmessage();
+		cout<<strmsg<<endl;
+	}else{
+		bf.open(name.c_str(), ios::out|ios::binary);
+		if (bf.is_open())
+		{
+			myt.set_id(102);
+			myt.set_longid(201);
+			myt.set_strmessage("这是个中文字符串！");
+			string data = myt.SerializeAsString();
+			const char* pdata = data.data();
+			int nsize = data.size();
+			bf.write(pdata, nsize);
+			myt.PrintDebugString();
+			cout<<myt.strmessage()<<endl;
+		}
+	}
 	return 0;
 }
 
