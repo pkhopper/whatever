26f85e2e5aed04870701b1b9b9055161d9aa045a
pkhopper <letian.llt@live.com>
2015-10-12 23:39:27 +0800

    static file service

--- pyproject/MiniWebServer/main.py
+++ pyproject/MiniWebServer/main.py
@@ -8,10 +8,12 @@ import sys
 import os.path as pathtool
 import web
 
-render = web.template.render('templates/')
 
+render = web.template.render('templates/')
 urls = (
     '/(.*)/', 'redirect',
+    '/res/(.*)', 'redirect_static',
+    '/static/(.*)', 'static_err',
     '/', 'index',
     '/hello', 'hellowode',
     '/h5', 'test_h5',
@@ -23,6 +25,17 @@ class redirect:
     def GET(self, path):
         web.seeother('/' + path)
 
+
+class redirect_static:
+    def GET(self, path):
+        web.seeother('/static/' + path)
+
+
+class static_err:
+    def GET(self, path):
+        return web.notfound()
+
+
 class index:
     def GET(self):
         return "this is index page"
@@ -33,11 +46,9 @@ class hellowode:
         data = web.input(name='Bob')
         return render.hellowode(data.name)
 
-
-app = web.application(urls, globals())
-
-
 if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.internalerror = web.debugerror
     app.run()
 
 
