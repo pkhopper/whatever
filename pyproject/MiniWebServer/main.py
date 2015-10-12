c11a9537d9560d072bbee47b766edb7808e40b85
pkhopper <letian.llt@live.com>
2015-10-12 23:01:32 +0800

    101 of web.py
    easy

---
+++ pyproject/MiniWebServer/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+__author__ = 'liletian'
+
+import os
+import sys
+import os.path as pathtool
+import web
+
+render = web.template.render('templates/')
+
+urls = (
+    '/(.*)/', 'redirect',
+    '/', 'index',
+    '/hello', 'hellowode',
+    '/h5', 'test_h5',
+)
+
+
+# http://webpy.org/cookbook/helloworld
+class redirect:
+    def GET(self, path):
+        web.seeother('/' + path)
+
+class index:
+    def GET(self):
+        return "this is index page"
+
+
+class hellowode:
+    def GET(self):
+        name = 'Bob'
+        return render.hellowode(name)
+
+
+app = web.application(urls, globals())
+
+
+if __name__ == "__main__":
+    app.run()
+
+
