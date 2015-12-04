4dc1c29465c622f8d5d7412677fb050c22cb5550
pkhopper <letian.llt@live.com>
2015-12-04 21:45:24 +0800

    miniwebserver

--- pyproject/MiniWebServer/main.py
+++ pyproject/MiniWebServer/main.py
@@ -11,34 +11,30 @@ import web
 
 render = web.template.render('templates/')
 urls = (
-    '/(.*)/', 'redirect',
+    '/', 'Index',
+    '/(.*)/', 'Index',
     '/res/(.*)', 'redirect_static',
-    '/static/(.*)', 'static_err',
-    '/', 'index',
-    '/hello', 'hellowode',
-    '/h5', 'test_h5',
 )
 
 
-# http://webpy.org/cookbook/helloworld
 class redirect:
-    def GET(self, path):
+    def GET(self, path=''):
         web.seeother('/' + path)
 
 
 class redirect_static:
-    def GET(self, path):
+    def GET(self, path=''):
         web.seeother('/static/' + path)
 
 
-class static_err:
-    def GET(self, path):
+class Index:
+    def GET(self, path=''):
+        return "" #web.notfound()
+
+
+class InternalErr:
+    def GET(self, messge=''):
         return web.notfound()
-
-
-class index:
-    def GET(self):
-        return "this is index page"
 
 
 class hellowode:
@@ -46,9 +42,8 @@ class hellowode:
         data = web.input(name='Bob')
         return render.hellowode(data.name)
 
+
 if __name__ == "__main__":
     app = web.application(urls, globals())
-    app.internalerror = web.debugerror
+    app.internalerror = web.BadRequest #.debugerror
     app.run()
-
-
