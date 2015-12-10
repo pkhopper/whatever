f9dc36d5a5df3a5575c99c2bc7813d15cf549ff5
pkhopper <letian.llt@live.com>
2015-12-10 18:11:09 +0800

    first

--- pyproject/MiniWebServer/main.py
+++ pyproject/MiniWebServer/main.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-__author__ = 'liletian'
+__author__ = 'pkhopper'
 
-import os
-import sys
-import os.path as pathtool
 import web
+from web import form
 
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base='layout')
 urls = (
-    '/', 'Index',
-    '/(.*)/', 'Index',
+    # '/(.*)/', 'Index',
     '/res/(.*)', 'redirect_static',
+    # pages
+    '/', 'PageIndex',
+    '/login', 'PageLogin',
+    '/logout', 'PageLogout',
+    '/query', 'PageQuery',
+    '/status', 'PageStatus',
+    '/viewlog', 'PageViewLog',
+    '/data', 'PageData',
 )
 
 
+allowed = (
+    ('aaa', '123123'),
+)
+
+
+web.config.debug = False
+app = web.application(urls, globals())
+app.internalerror = web.BadRequest #.debugerror
+session = web.session.Session(app, web.session.DiskStore('sessions'))
+
+
+# default act
 class redirect:
     def GET(self, path=''):
         web.seeother('/' + path)
@@ -27,23 +44,74 @@ class redirect_static:
         web.seeother('/static/' + path)
 
 
-class Index:
-    def GET(self, path=''):
-        return "" #web.notfound()
-
-
 class InternalErr:
     def GET(self, messge=''):
         return web.notfound()
 
 
-class hellowode:
+# pages
+class PageIndex:
+    def GET(self, path=None):
+        if not session.logged_in:
+            raise web.seeother('/login')
+        return render.PageIndex()
+
+
+class PageLogin:
     def GET(self):
-        data = web.input(name='Bob')
-        return render.hellowode(data.name)
+        return render.PageLogin()
+
+    def POST(self):
+        i = web.input()
+        username = i.get('username')
+        passwd = i.get('passwd')
+        if (username, passwd) in allowed:
+            session.logged_in = True
+            web.setcookie('system_mangement', '', 60)
+            return web.seeother('/')
+        else:
+            return web.seeother("/login")
+
+
+class PageLogout:
+    def GET(self):
+        session.logged_in = False
+        return web.seeother("/login")
+
+
+class PageQuery:
+    def GET(self):
+        if not session.logged_in:
+            return web.seeother('/')
+        return render.PageQuery()
+    # def GET(self, path):
+    #     data = web.input(path="")
+    #     return render.PageQuery(data)
+
+
+class PageStatus:
+    def GET(self):
+        if not session.logged_in:
+            return web.seeother('/')
+        return render.PageStatus()
+
+
+class PageViewLog:
+    def GET(self):
+        if not session.logged_in:
+            return web.seeother('/')
+        return web.seeother('/static/log/')
+        # return render.PageViewLog()
+
+
+class PageData:
+    def GET(self):
+        if not session.logged_in:
+            return web.seeother('/')
+        return web.seeother('/static/db/')
+        # return render.PageViewLog()
+
 
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.internalerror = web.BadRequest #.debugerror
     app.run()
